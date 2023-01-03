20c3327d6c0d3eabe21da456f32c79f534ad67dd
Damir <dtemi2@uis.edu>
2023-01-02 20:41:31 -0600

    feat: use GitHub token for higher API limits

--- go.mod
+++ go.mod
@@ -2,9 +2,17 @@ module github.com/dtemir/git-badges-golang
 
 go 1.19
 
-require github.com/google/go-github/v48 v48.2.0
+require (
+	github.com/google/go-github/v48 v48.2.0
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/oauth2 v0.3.0
+)
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.3.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,36 +5,53 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	"github.com/google/go-github/v48/github"
+	"golang.org/x/oauth2"
 )
 
 // Badge with the number of GitHub Organizations
-func organizationHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/organizations" {
-		http.Error(w, "404 not found", http.StatusNotFound)
+func organizationHandler(client github.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/organizations" {
+			http.Error(w, "404 not found", http.StatusNotFound)
+		}
+		if r.Method != "GET" {
+			http.Error(w, "method is not supported", http.StatusNotFound)
+		}
+
+		// Get GitHub username, i.e. /organizations?username=dtemir
+		username := r.URL.Query().Get("username")
+
+		// Fetch the number of Organizations the user has
+		// docs.github.com/en/rest/orgs/orgs?apiVersion=2022-11-28#list-organizations-for-a-user
+		orgs, _, err := client.Organizations.List(context.Background(), username, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Fprintf(w, "Number of organizations for %s is %d", username, len(orgs))
 	}
-	if r.Method != "GET" {
-		http.Error(w, "method is not supported", http.StatusNotFound)
-	}
-
-	// Get GitHub username, i.e. /organizations?username=dtemir
-	username := r.URL.Query().Get("username")
-
-	client := github.NewClient(nil)
-
-	// Fetch the number of Organizations the user has
-	// docs.github.com/en/rest/orgs/orgs?apiVersion=2022-11-28#list-organizations-for-a-user
-	orgs, _, err := client.Organizations.List(context.Background(), username, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Fprintf(w, "Number of organizations for %s is %d", username, len(orgs))
 }
 
 func main() {
-	http.HandleFunc("/organizations", organizationHandler)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	http.HandleFunc("/organizations", organizationHandler(*client))
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
