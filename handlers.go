3a3dbbb307ed83a6e9f1f062fd7a31bdd9709030
Damir <dtemi2@uis.edu>
2023-01-07 17:21:02 -0600

    feat: use MongoDB for storing the number of visits

--- handlers.go
+++ handlers.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/google/go-github/v48/github"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -147,8 +148,6 @@ func reposHandler(client github.Client) http.HandlerFunc {
 	}
 }
 
-var visits = make(map[string]int)
-
 func visitsHandler(client github.Client, collection mongo.Collection) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/visits" {
@@ -160,7 +159,6 @@ func visitsHandler(client github.Client, collection mongo.Collection) http.Handl
 
 		values := r.URL.Query()
 		username := values.Get("username")
-		repo := username + "/" + values.Get("repo")
 
 		color := values.Get("color")
 		if len(color) == 0 {
@@ -171,9 +169,48 @@ func visitsHandler(client github.Client, collection mongo.Collection) http.Handl
 
 		logo := values.Get("logo")
 
-		visits[repo] = visits[repo] + 1
+		// Get a repo name, i.e. dtemir/github-badges-golang
+		repo := username + "/" + values.Get("repo")
 
-		url := fmt.Sprintf("https://img.shields.io/badge/Visits-%d-%s?style=%s&logo=%s", visits[repo], color, style, logo)
+		var visits int64
+
+		// Find the document to update
+		var err error
+		var doc bson.M
+		filter := bson.M{"name": repo}
+		err = collection.FindOne(context.Background(), filter).Decode(&doc)
+		if err != nil {
+			// No document exist, i.e. first time visit
+			if err == mongo.ErrNoDocuments {
+				visits = 1
+				// Create a new document with one visit (make sure it is int64)
+				doc = bson.M{"name": repo, "visits": int64(visits)}
+				_, err = collection.InsertOne(context.Background(), doc)
+				if err != nil {
+					log.Fatal("MongoDB: Failed to very first visit\n %w", err)
+				}
+			} else {
+				log.Fatal("MongoDB: Couldn't find the document to update\n %w", err)
+			}
+		} else {
+			// Increment the number of visits by one
+			v, ok := doc["visits"].(int64)
+			if !ok {
+				fmt.Println("Error converting 'visits' field to int64")
+				return
+			}
+			doc["visits"] = v + 1
+
+			visits = v + 1
+
+			// Update the document in the collection with a new count
+			_, err = collection.ReplaceOne(context.Background(), filter, doc)
+			if err != nil {
+				log.Fatal("MongoDB: Couldn't update the document in the collection")
+			}
+		}
+
+		url := fmt.Sprintf("https://img.shields.io/badge/Visits-%d-%s?style=%s&logo=%s", visits, color, style, logo)
 
 		svg := getSVG(url)
 
